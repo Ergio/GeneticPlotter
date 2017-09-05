51ef0b4ffe5b6e2440eee762015ba7a8a9f8ea16
Serhii Kulaha <serhiikulaha@gmail.com>
2017-09-05 19:20:32 +0300

    new UI design

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QStyleFactory
-from view_model.main_view_model import MainViewModel 
+from genplotter import GenPlotter 
 from PyQt5.QtCore import Qt, QDir,QFileInfo, QFile, QTextStream
-from PyQt5.QtGui import QKeySequence, QIcon, QPalette, QColor
-
+import resources
 
 if __name__ == '__main__':
 
@@ -11,11 +10,11 @@ if __name__ == '__main__':
     app.setStyle(QStyleFactory.create("Fusion"))
 
  # setup stylesheet
-    file = QFile("light.qss")
+    file = QFile(":resources/light_style.qss")
     file.open(QFile.ReadOnly | QFile.Text)
     stream = QTextStream(file)
     app.setStyleSheet(stream.readAll())
 
-    ex = MainViewModel()
+    ex = GenPlotter()
     sys.exit(app.exec_())
     
--- model/toolbar.py
+++
@@ -1,8 +0,0 @@
-from widgets.toolbar_widget import ToolBarWidget
-from PyQt5.QtCore import Qt
-
-class ToolBar(ToolBarWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.setContextMenuPolicy(Qt.NoContextMenu)
